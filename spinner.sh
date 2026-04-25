95828761417608dc5591fbbd6903dfbd0cd85b21
tgnlex <alexanderwebber76@gmail.com>
2026-04-25 12:20:39 -0400

    auto-commit

--- spinner.sh
+++ spinner.sh
@@ -1,6 +1,11 @@
 spinner() {
-  for c in '/' '|' '\' '-'; do 
-    printf '\r%s' "$c"
-    sleep 1
-  done 
+  local c 
+  while true; do
+    for c in ' / ' ' | ' ' \ ' ' - '; do 
+      printf '\r%s' "$c"
+      sleep 0.1
+    done 
+  done
+  
 }
+spinner
